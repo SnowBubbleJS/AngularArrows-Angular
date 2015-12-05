62d63f1c711a3c41b0fdaa3d2f45afa2a9430aad
Bryan <bhtruong93@gmail.com>
2015-12-04 17:29:26 -0800

    more completed

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -2,7 +2,7 @@ angular
   .module('app')
   .controller('htmlController', myTextArea);
 
-function myTextArea($scope) {
+function myTextArea($scope, output) {
   $scope.editorOptions = {
       lineWrapping : true,
       lineNumbers: true,
@@ -11,9 +11,12 @@ function myTextArea($scope) {
   };
   $scope.codemirrorLoaded = function(_editor) {
     _editor.setValue('<div>HTML</div>');
+    output.html = _editor.getValue();
+    output.render();
     _editor.on('change', function(inst, changes) {
-      console.log(inst);
-      console.log(_editor.getValue());
+      output.html = _editor.getValue();
+      console.log(output.html);
+      output.render();
     });
   };
 }
--- factory.js
+++ factory.js
@@ -2,6 +2,48 @@ angular
   .module('app')
   .factory('output', output);
 
-  function output() {
-    
-  }
+function output() {
+  var out = {
+    html: '',
+    partial: '',
+    app: '',
+    controller: '',
+    prepareSource: function() {
+      var base_tpl =
+        "<!doctype html>\n" +
+        "<html>\n\t" +
+        "<head>\n\t\t" +
+        "<meta charset=\"utf-8\">\n\t\t" +
+        "<title>Test</title>\n\n\t\t\n\t" +
+        "</head>\n\t" +
+        "<body>\n\t\n\t" +
+        "</body>\n" +
+        "</html>";
+      var src = '';
+      // HTML
+      src = base_tpl.replace('</body>', this.html + '</body>');
+
+      // Partial
+      // css = '<html>' + this.partial + '</html>';
+      // src = src.replace('</div>', this.partial + '</div>');
+
+      // App
+      appScript = '<script>' + this.app + '<\/script>';
+      src = src.replace('</body>', appScript + '</body>');
+
+      // Controller
+      controllerScript = '<script>' + this.controller + '<\/script>';
+      src = src.replace('</body>', controllerScript + '</body>');
+      return src;
+    },
+    render: function() {
+      var source = this.prepareSource();
+      var iframe = document.querySelector('#output iframe');
+      iframe_doc = iframe.contentDocument;
+      iframe_doc.open();
+      iframe_doc.write(source);
+      iframe_doc.close();
+    }
+  };
+  return out;
+}
