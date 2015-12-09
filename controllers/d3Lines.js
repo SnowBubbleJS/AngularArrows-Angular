aeec77bbcc39138a8c0d54d15cee467460530244
Bryan <bhtruong93@gmail.com>
2015-12-08 16:23:16 -0800

    k

--- controllers/d3Lines.js
+++ controllers/d3Lines.js
@@ -4,15 +4,13 @@ angular
 
 function lineFunction(query){
   var temp = $(query);
+  var c=document.getElementById("canvas");
+  var ctx=c.getContext("2d");
   if(temp.length > 1) {
     var x1 = $(temp[0]).offset().left+5;
     var y1 = $(temp[0]).offset().top +10;
     var x2 = $(temp[1]).offset().left+5;
     var y2 = $(temp[1]).offset().top +10;
-
-
-    var c=document.getElementById("canvas");
-    var ctx=c.getContext("2d");
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     ctx.beginPath();
     ctx.lineWidth = 0.2;
@@ -20,7 +18,9 @@ function lineFunction(query){
     ctx.quadraticCurveTo(400,400,x2,y2);
     ctx.stroke();
   }
-
+  if(temp.length < 2) {
+      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  }
 
 
 }
