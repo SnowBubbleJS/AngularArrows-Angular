d5f47fd5b407e66e018adfe8efa2c203c5f53c17
danieldealday <danieldealday@gmail.com>
2015-12-11 15:05:40 -0800

    added ui-bootstrap.tpls-0.12.0.js for version correct ui-bootstrap, tutorialContent.html, and modalCtrl.js

--- modalCtrl.js
+++ modalCtrl.js
@@ -1,51 +1,40 @@
-angular.module('app').controller('modalCtrl', function ($scope, $uibModal, $log) {
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
+angular
+  .module('app')
+  .controller('modalCtrl', function ($scope, $modal, $log) {
+    $scope.items = ['item1', 'item2', 'item3'];
+    $scope.open = function (size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'tutorialContent.html',
+        controller: 'modalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
         }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  // $scope.toggleAnimation = function () {
-  //   $scope.animationsEnabled = !$scope.animationsEnabled;
-  // };
-
-});
+      });
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
+// It is not the same as the $modal service used above.
 
-angular.module('apps').controller('modalInstanceCtrl', function ($scope, $uibModalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-});
+angular
+  .module('app')
+    .controller('modalInstanceCtrl', function ($scope, $modalInstance, items) {
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
