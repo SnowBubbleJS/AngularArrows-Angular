a68554d08ab5e8e4dc89c640aa32634f27c4f325
mlee277 <mlee277@gmail.com>
2015-12-16 13:26:37 -0800

    factory works

--- controllers/factoryController.js
+++ controllers/factoryController.js
@@ -17,7 +17,6 @@
     ////////////
 
     function codemirrorLoaded(_editor) {
-      _editor.setValue();
       output.factory = _editor.getValue();
       output.render();
       _editor.on('change', function(inst, changes) {
