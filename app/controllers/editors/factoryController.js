6163b63e7e867426a7f47f07b526249885bacc0f
Bryan <bhtruong93@gmail.com>
2015-12-16 13:25:58 -0800

    reorder complete, factory works

--- app/controllers/editors/factoryController.js
+++ app/controllers/editors/factoryController.js
@@ -17,10 +17,10 @@
     ////////////
 
     function codemirrorLoaded(_editor) {
-      _editor.setValue();
       output.factory = _editor.getValue();
       output.render();
       _editor.on('change', function(inst, changes) {
+        console.log('im here');
         output.factory = _editor.getValue();
         output.render();
       });
