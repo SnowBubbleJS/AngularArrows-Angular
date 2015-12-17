cebe3696e0f3612a0515461ef11795041cd5dd97
Bryan <bhtruong93@gmail.com>
2015-12-17 10:33:59 -0800

    works need to put new modal

--- app/factories/factory.js
+++ app/factories/factory.js
@@ -2,9 +2,9 @@ angular
   .module('app')
   .factory('output', output);
 
-output.$inject = ["promptFactory","inputFactory"];
+output.$inject = ["$rootScope", "promptFactory","inputFactory"];
 
-function output(promptFactory, inputFactory) {
+function output($rootScope, promptFactory, inputFactory) {
   var out = {
     html: '',
     partial: '',
@@ -54,7 +54,7 @@ function output(promptFactory, inputFactory) {
         this.source = this.prepareSource();
         console.log(this.source);
         if(!!inputFactory.checkInput(this.source)) {
-          promptFactory.shouldUpdate = 1;
+          $rootScope.$broadcast('answer:correct', promptFactory.counter);
           console.log('goodjob..should update');
         }
 
