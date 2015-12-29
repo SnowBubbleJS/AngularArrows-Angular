9026b7215e57e5e4fb263c56c1a32d525a317b88
danieldealday <danieldealday@gmail.com>
2015-12-29 12:51:56 -0800

    editted app.js and broke up prompts to track tasks in paragraphs more easily

--- app/app.js
+++ app/app.js
@@ -7,17 +7,14 @@ angular
   })
   .directive('menuKey', function() {
       return function(scope, element, attrs) {
-
           element.bind("keydown keypress", function(event) {
               var keyCode = event.which || event.keyCode;
-
               // If enter key is pressed
               if (keyCode === 27) {
                   scope.$apply(function() {
                           // Evaluate the expression
                       scope.$eval(attrs.menuKey);
                   });
-
                   event.preventDefault();
               }
           });
