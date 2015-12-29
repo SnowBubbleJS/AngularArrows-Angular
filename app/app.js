fda820b61d6b498a5181665ee0aa6291d7675417
danieldealday <danieldealday@gmail.com>
2015-12-29 12:18:00 -0800

    added tooltips and Esc button to open sidenav

--- app/app.js
+++ app/app.js
@@ -4,4 +4,23 @@ angular
   $mdThemingProvider.theme('default')
     .primaryPalette('blue')
     .accentPalette('orange');
-});
+  })
+  .directive('menuKey', function() {
+      return function(scope, element, attrs) {
+
+          element.bind("keydown keypress", function(event) {
+              var keyCode = event.which || event.keyCode;
+
+              // If enter key is pressed
+              if (keyCode === 27) {
+                  scope.$apply(function() {
+                          // Evaluate the expression
+                      scope.$eval(attrs.menuKey);
+                  });
+
+                  event.preventDefault();
+              }
+          });
+      };
+  })
+;
