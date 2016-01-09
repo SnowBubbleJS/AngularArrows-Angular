b8e9060310d8c91eec06b3453421da95bb899ae8
bhtruong93 <bhtruong93@gmail.com>
2016-01-08 17:03:08 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -16,3 +16,40 @@
       };
   })
 ;
+
+
+  .directive('menuKey', function() {
+      return function(scope, element, attrs) {
+          element.bind("keydown keypress", function(event) {
+              var keyCode = event.which || event.keyCode;
+              // If enter key is pressed
+              if (keyCode === 27) {
+
+                  scope.$apply(function() {
+                          // Evaluate the expression
+                      scope.$eval(attrs.menuKey);
+                  });
+                  event.preventDefault();
+              }
+          });
+      };
+  })
+;
+
+  .directive('menuKey', function() {
+      return function(scope, element, attrs) {
+          element.bind("keydown keypress", function(event) {
+              var keyCode = event.which || event.keyCode;
+              // If enter key is pressed
+              if (keyCode === 27) {
+
+                  scope.$apply(function() {
+                          // Evaluate the expression
+                      scope.$eval(attrs.menuKey);
+                  });
+                  event.preventDefault();
+              }
+          });
+      };
+  })
+;
