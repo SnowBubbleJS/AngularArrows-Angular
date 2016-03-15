817812bc8f0fc1be3615b7565a23d10b8a687a8a
Bryan <bhtruong93@gmail.com>
2016-03-14 18:34:19 -0700

    added simple tests

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -7,3 +7,24 @@ describe('SnowBubble app', function() {
     expect(browser.getTitle()).toEqual('SnowBubble');
   });
 });
+
+describe('Editors loaded', function() {
+  it('should have html editor', function() {
+    var html = element(by.id('html'));
+    expect(html);
+  });
+  it('should have app editor', function() {
+    var app = element(by.id('app'));
+    expect(app);
+  });
+
+  it('should have controller editor', function() {
+    var controller = element(by.id('controller'));
+    expect(controller);
+  });
+
+  it('should have factory editor', function() {
+    var factory = element(by.id('factory'));
+    expect(factory);
+  });
+});
