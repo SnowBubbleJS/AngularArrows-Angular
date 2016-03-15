dbf4abab48ca18df76b801348615a27b1f6c1e6f
Bryan <bhtruong93@gmail.com>
2016-03-14 18:26:12 -0700

    cleaning up

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -1,25 +1,9 @@
-// describe('angularjs homepage todo list', function() {
-//   it('should add a todo', function() {
-//     browser.get('https://angularjs.org');
-//
-//     element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-//     element(by.css('[value="add"]')).click();
-//
-//     var todoList = element.all(by.repeater('todo in todoList.todos'));
-//     expect(todoList.count()).toEqual(3);
-//     expect(todoList.get(2).getText()).toEqual('write first protractor test');
-//
-//     // You wrote your first test, cross it off the list
-//     todoList.get(2).element(by.css('input')).click();
-//     var completedAmount = element.all(by.css('.done-true'));
-//     expect(completedAmount.count()).toEqual(2);
-//   });
-// });
-
+/* Angular material doesn't work well with protractor...
+  https://github.com/angular/material/issues/1063
+*/
 describe('SnowBubble app', function() {
   it('should have a title', function() {
-      browser.get('http://localhost:5000/');
-      expect(browser.getTitle()).toEqual('SnowBubble');
+    browser.get('http://localhost:5000/');
+    expect(browser.getTitle()).toEqual('SnowBubble');
   });
-
 });
